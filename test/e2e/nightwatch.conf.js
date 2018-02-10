6fcb8277f5c27d221220abcac43be5106bcbc544
Mark Meyer <mark@photo-mark.com>
2018-02-09 16:07:08 -0900

    e2e tests only use chromedriver

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -1,27 +1,37 @@
 require('babel-register')
 var config = require('../../config')
-
+var chromedrive = require('chromedriver')
 // http://nightwatchjs.org/gettingstarted#settings-file
 module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
-
+  globals_path: 'test/e2e/globals.js',
+  /* selenum is difficult to run from node on travis ci
+     so this just uses the stand alone chrome driver */
   selenium: {
-    start_process: true,
+    start_process: false,
     server_path: require('selenium-server').path,
     host: '127.0.0.1',
     port: 4444,
     cli_args: {
-      'webdriver.chrome.driver': require('chromedriver').path
+      'webdriver.chrome.driver': chromedrive.path
     }
   },
 
   test_settings: {
     default: {
-      selenium_port: 4444,
+      selenium_port: 9515,
       selenium_host: 'localhost',
+      default_path_prefix: "",
       silent: true,
+      desiredCapabilities: {
+        browserName: "chrome",
+        chromeOptions : {
+          args : ["--no-sandbox"]
+        },
+        acceptSslCerts: true
+      },
       globals: {
         devServerURL: 'http://localhost:' + (process.env.PORT || config.dev.port)
       }
@@ -30,14 +40,6 @@ module.exports = {
     chrome: {
       desiredCapabilities: {
         browserName: 'chrome',
-        javascriptEnabled: true,
-        acceptSslCerts: true
-      }
-    },
-
-    firefox: {
-      desiredCapabilities: {
-        browserName: 'firefox',
         javascriptEnabled: true,
         acceptSslCerts: true
       }
