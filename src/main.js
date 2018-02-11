a1d347537bedf973f102efc190e42c0271f6fb29
Mark Meyer <mark@photo-mark.com>
2018-02-10 19:16:55 -0900

    One more time, back to babel-polyfill of Promises fro Internet Explorer

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import 'es6-promise/auto'; // axios depends on native promises. This adds polyfill for internet explorer
+
 import Vue from 'vue'
 import App from './App'
 import router from './router'
