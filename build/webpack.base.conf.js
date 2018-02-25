b0eb1fc2ddfe85371708ff1681af1362c787336c
Mark Meyer <mark@photo-mark.com>
2018-02-25 10:26:50 -0900

    prevent moment from loading locales files -- this saves ~200K in final bundle

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+var webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -65,5 +66,9 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    // keep bundle small by excluing moment's locales files 
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+  ]
 }
